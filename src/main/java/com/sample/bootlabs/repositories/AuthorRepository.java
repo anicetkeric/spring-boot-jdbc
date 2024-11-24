66775af219e84bf165ed96fbded568a0e6c7f121
aek <anicetkeric@outlook.com>
2024-11-24 13:49:37 +0100

    impl JdbcClient with AuthorRepository

--- src/main/java/com/sample/bootlabs/repositories/AuthorRepository.java
+++ src/main/java/com/sample/bootlabs/repositories/AuthorRepository.java
@@ -2,9 +2,7 @@ package com.sample.bootlabs.repositories;
 
 import com.sample.bootlabs.model.Author;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,54 +15,54 @@ import java.util.Optional;
 @Repository
 public class AuthorRepository implements JdbcRepository<Author> {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    RowMapper<Author> rowMapper = (res, rowNum) -> Author.builder().
-            id(res.getLong("id"))
-            .firstname(res.getString("firstname"))
-            .lastname(res.getString("lastname")).build();
-
+    private final JdbcClient jdbcClient;
 
     @Override
     public Author save(Author author) {
         var query = "INSERT INTO author(id, firstname, lastname) values(?,?,?)";
-        int inserted = jdbcTemplate.update(query, author.id(), author.firstname(), author.lastname());
+        int inserted = jdbcClient.sql(query)
+                .params(author.id(), author.firstname(), author.lastname())
+                .update();
         if (inserted == 1) {
             return author;
         }
-        return null;
+      throw new RuntimeException("Save failed");
     }
 
     @Override
     public Author update(Author author, Long id) {
         var query = "UPDATE author set firstname = ?, lastname = ? where id = ?";
-        int updated = jdbcTemplate.update(query, author.firstname(), author.lastname(), id);
+        int updated = jdbcClient.sql(query)
+                .params(author.firstname(), author.lastname(), id)
+                .update();
         if (updated == 1) {
             return author;
         }
-        return null;
+        throw new RuntimeException("Update failed");
     }
 
     @Override
     public List<Author> findAll() {
         var query = "SELECT id,firstname,lastname FROM author";
-        return jdbcTemplate.query(query, rowMapper);
+        return jdbcClient.sql(query)
+                .query(Author.class)
+                .list();
     }
 
     @Override
     public Optional<Author> findById(Long id) {
-        var query = "SELECT id, firstname, lastname FROM author where id = ?";
-        try {
-            Author author = jdbcTemplate.queryForObject(query, rowMapper, id);
-            return Optional.ofNullable(author);
-        } catch (DataAccessException e) {
-            return Optional.empty();
-        }
+        var query = "SELECT id, firstname, lastname FROM author where id = :id";
+        return jdbcClient.sql(query)
+                .param("id", id)
+                .query(Author.class)
+                .optional();
     }
 
     @Override
     public int deleteById(Long id) {
-        var query = "DELETE from author where id = ?";
-        return jdbcTemplate.update(query, id);
+        var query = "DELETE from author where id = :id";
+        return jdbcClient.sql(query)
+                .param("id", id)
+                .update();
     }
 }
